0d6dd807cacc74907be460b29b431489ccc3fa84
hakaneverdi <73536509+hakaneverdi@users.noreply.github.com>
2023-06-01 01:52:09 +0300

    odevler

---
+++ Hafta1/artik_yil/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Yılı Girin: ");
+        int yil = scanner.nextInt();
+
+        if ((yil % 4 == 0 && yil % 100 != 0) || yil % 400 == 0) {
+            System.out.println(yil + " artık bir yıldır.");
+        } else {
+            System.out.println(yil + " artık bir yıl değildir.");
+        }
+    }
+}
